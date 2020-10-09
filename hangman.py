70c2af0ea6d7e414c0da28e215da6407e4b59b57
OzanColtekin <72617899+OzanColtekin@users.noreply.github.com>
2020-10-09 19:48:11 +0300

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,33 @@
+from random import randint
+index = randint(0,999)
+with open("words.txt","r") as words:
+    words_file = words.read().split("\n")
+word = words_file[index]
+guess_list = []
+name = input("Enter your name: ")
+print("Welcome " + name + " to play hangman!")
+if len(word) > 5:
+    lives = 20
+else:
+    lives = 10
+
+while lives >0:
+    checkpoint = len(word)
+    for letter in word:
+        if letter in guess_list:
+            print(letter)
+            checkpoint -= 1
+        else:
+            print("*")
+    if checkpoint ==0:
+        print("You won!")
+        break
+    guess = input("Give a letter: ")
+    guess_list += guess
+    if guess not in word:
+        print("Wrong letter try again!")
+        lives -=1
+        if lives !=0:
+            print(f"You have {lives} left")
+    if lives == 0:
+        print("Game over!")
